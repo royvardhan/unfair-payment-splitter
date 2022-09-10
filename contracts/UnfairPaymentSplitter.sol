3050f25f66536ff03a0e1933c8350888c9e4c352
royvardhan <royvardhan.official@gmail.com>
2022-09-10 11:43:51 +0530

    Imported OZ dependency and added constructor

--- contracts/UnfairPaymentSplitter.sol
+++ contracts/UnfairPaymentSplitter.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+
+contract UnfairPaymentSplitter is PaymentSplitter {
+    constructor(address[] memory _payees, uint256[] memory _shares) PaymentSplitter(_payees, _shares) payable {
+
+    }
+}
